3f23a9a32f9a89e9a2aed3db5e62c07224ea4fca
liyongchang <liyongchang0001@126.com>
2018-10-31 17:12:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sparkDataProcess
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,29 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @Auther: liyongchang
+  * @Date: 2018/10/31 15:25
+  * @Description:
+  */
+object Test {
+
+  val conf = new SparkConf().setMaster("local").setAppName("test")
+  val sc = new SparkContext(conf)
+
+  def main(args: Array[String]):Unit = {
+    //    val fileName = "2015-01.csv"
+    //    val inputPath = "hdfs://masters/solap/airData/" + fileName
+//    val inputPath = "C:\\Users\\liyongchang\\IdeaProjects\\learnScala\\data\\2015-01.csv"
+//    val input = sc.textFile(inputPath)
+    val input = sc.parallelize(List(1,2,3,4,5,6))
+    //缓存，一个rdd进行多次action操作时可使用
+    input.persist()
+    println(input.count())
+    //take获取rdd的前n行数据
+    //input.take(100).foreach(println)
+    input.collect().foreach(println)
+    //input.saveAsTextFile("C:\\Users\\liyongchang\\IdeaProjects\\learnScala\\data\\2015-01_out")
+
+  }
+
+}
