da2b4082dbcd5070e4041fdd7f9904edc5b70ca0
yongchang li <liyongchang0001@126.com>
2018-12-11 16:13:51 +0800

    hbase

--- src/main/scala/com/data/database/HBaseClient.scala
+++ src/main/scala/com/data/database/HBaseClient.scala
@@ -1,5 +1,25 @@
 package com.data.database
 
-class HBaseClient {
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.{HTable, Put}
+import org.apache.hadoop.hbase.util.Bytes
+
+object HBaseClient {
+
+  def main(args: Array[String]): Unit = {
+    val conf = HBaseConfiguration.create()
+
+    conf.set("hbase.zookeeper.quorum", "master, dell4, xiadclinux")
+
+    val table = new HTable(conf, "testtable")
+
+    val put = new Put(Bytes.toBytes("row1"))
+
+    put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("val1"))
+    put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"), Bytes.toBytes("val2"))
+
+    table.put(put)
+
+  }
 
 }
