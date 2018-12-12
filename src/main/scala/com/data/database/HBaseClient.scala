4ef04e9fba1e9936add6f436b8f9ed348c53d941
yongchang li <liyongchang0001@126.com>
2018-12-12 16:10:57 +0800

    hbase select

--- src/main/scala/com/data/database/HBaseClient.scala
+++ src/main/scala/com/data/database/HBaseClient.scala
@@ -1,24 +1,41 @@
 package com.data.database
 
 import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.hbase.client.{HTable, Put}
+import org.apache.hadoop.hbase.client.{Get, HTable, Put}
 import org.apache.hadoop.hbase.util.Bytes
+
+import scala.reflect.internal.util.TableDef.Column
 
 object HBaseClient {
 
+  val conf = HBaseConfiguration.create()
+
+
+  def selectOneColumnByRowKey(tableName: String, rowKey: Array[Byte], family: Array[Byte], column: Array[Byte]): Array[Byte] ={
+    val table = new HTable(conf, tableName)
+    val get = new Get(rowKey)
+    get.addColumn(family, column)
+    val result = table.get(get)
+    val value = result.getValue(family, column)
+    value
+  }
+
+
   def main(args: Array[String]): Unit = {
-    val conf = HBaseConfiguration.create()
+
 
     //conf.set("hbase.zookeeper.quorum", "master, dell4, xiadclinux")
 
-    val table = new HTable(conf, "testtable")
-
-    val put = new Put(Bytes.toBytes("row1"))
-
-    put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("val1"))
-    put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"), Bytes.toBytes("val2"))
-
-    table.put(put)
+//    val table = new HTable(conf, "testtable")
+//
+//    val put = new Put(Bytes.toBytes("row1"))
+//
+//    put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("val1"))
+//    put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"), Bytes.toBytes("val2"))
+//
+//    table.put(put)
+    val value = selectOneColumnByRowKey("testtable", Bytes.toBytes("row1"), Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"))
+    print(Bytes.toString(value))
 
   }
 
